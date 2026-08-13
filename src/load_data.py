21d95e70497925b079524412a885b190ea228116
amnakhalid87 <amnakhalid8791@gmail.com>
2026-08-12 22:46:35 -0700

    load dataset from hugging face hub

--- src/load_data.py
+++ src/load_data.py
@@ -1,6 +1,13 @@
+import os
 import pandas as pd
-
+from huggingface_hub import hf_hub_download
 
 def load_data(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path, encoding="latin1")
-    return df
+
+    repo_file_path = hf_hub_download(
+        repo_id="amnaakhalid1/Netflix_data",
+        filename="netflix_data.csv.zip",
+        repo_type="dataset"
+    )
+    
+    return pd.read_csv(repo_file_path, encoding="latin1")
